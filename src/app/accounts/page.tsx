101710bba0f48270c8f51fb06b8a527ca5c43f30
Tobias Vinsa <tobias.vinsa@senarion.com>
2024-11-22 15:20:36 +0100

    Added loading and error check to account page

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -7,22 +7,40 @@ import { AccountColumns } from "./components/AccountColumns";
 
 export default function AccountsPage() {
   const [accounts, setAccounts] = useState<Account[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
     const fetchAccounts = async () => {
-      const response = await fetch("https://api.atlar.com/financial-data/v2/accounts"
-        , {
-        method: "GET",
-        headers: {
-          "Authorization": "Basic " + btoa('AvahI5yjCWUV43vu67Ka:oRpiDBaluISG0aSGhu8a_5_WQ3x0oNb9Ggr51G--TgJ7wSQb5k9LxkpLE3BMWAhf20ei'),
-        },
-      });
-      const data = await response.json();
-      setAccounts(data.items);
+      try {
+        setLoading(true);
+        
+        const response = await fetch("https://api.atlar.com/financial-data/v2/accounts"
+          , {
+          method: "GET",
+          headers: {
+            "Authorization": "Basic " + btoa('AvahI5yjCWUV43vu67Ka:oRpiDBaluISG0aSGhu8a_5_WQ3x0oNb9Ggr51G--TgJ7wSQb5k9LxkpLE3BMWAhf20ei'),
+          },
+        });
+        const data = await response.json();
+        setAccounts(data.items);
+      } catch (err: any) {
+        setError(err.message || "An error occurred");
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchAccounts();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div>
